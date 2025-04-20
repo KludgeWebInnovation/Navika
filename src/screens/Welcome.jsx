1e9c2acf365ece80ab58fc75a5bb7a627001a614
Arno <vdmarno@gmail.com>
2025-04-20 16:46:43 +0200

    Add files via upload

--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold text-orange-500">Welcome Screen</h1>
+      <p>This is the welcome page of Navika.</p>
+    </div>
+  );
+}
+
+export default Welcome;
